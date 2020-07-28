86f5d0ae7f89413de9ef14d188889c4be22eb074
Begana Choi <begana001.jpg@gmail.com>
2020-07-28 12:12:51 +0200

    Updates

---
+++ precache-manifest.08a10c6f9972882157d5b830af372945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be05e59ef65a04fd5eac",
+    "url": "/course-directory/static/css/main.d0282da0.chunk.css"
+  },
+  {
+    "revision": "be05e59ef65a04fd5eac",
+    "url": "/course-directory/static/js/main.be05e59e.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "60134f86cffe7fe492898c912abb6e4e",
+    "url": "/course-directory/index.html"
+  }
+];
