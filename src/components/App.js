95d99c8cd0336bb2c519c438e35851d63fc5e5c7
Begana Choi <begana001.jpg@gmail.com>
2020-07-28 12:27:39 +0200

    deploying HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <HashRouter hashType="noslash">
+  <HashRouter>
     <div className="container">
       <Header />
       
